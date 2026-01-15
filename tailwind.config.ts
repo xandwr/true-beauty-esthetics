86980b49d2bb48d3fda67240a2c3bdd7f00d8585
Xander Pickering <xandwrp@gmail.com>
2026-01-15 14:26:04 -0700

    Initial work on final dist

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"DM Sans"', "sans-serif"],
+      },
+    },
+  },
+} satisfies Config;
