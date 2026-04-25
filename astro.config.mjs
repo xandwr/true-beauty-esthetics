3518173f243c0b264681c41b1db4922c75c5cfd5
Xander Pickering <xandwrp@gmail.com>
2026-04-25 09:00:31 -0600

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://truebeautyesthetics.ca",
+  trailingSlash: "always",
 
   vite: {
     plugins: [tailwindcss()],
